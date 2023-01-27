f68d177dd66d9e6eade08bcf90c15f4ef4682589
highoncodes1 <highoncodes@gmail.com>
2023-01-27 11:21:19 +0100

    forms validation using regex

--- src/app.js
+++ src/app.js
@@ -5,6 +5,11 @@ hamburger.addEventListener("click", () => {
   navLink.classList.toggle("hide");
 });
 
+//Form
+document.getElementById('form').addEventListener('submit', function () {
+  alert("THANK YOU!");
+}, false);
+
 // Owlcarousel
 $(document).ready(function () {
   var owl = $(".owl-carousel");
@@ -41,3 +46,25 @@ $(document).ready(function () {
     owl.trigger("stop.owl.autoplay");
   });
 });
+
+//submit btn
+function handleSubmit(event) {
+  event.preventDefault(); // prevent the form from refreshing the page
+  const form = event.target;
+  const inputs = form.querySelectorAll("input");
+  let isValid = true;
+
+  // validate form inputs
+  inputs.forEach((input) => {
+    if (!input.checkValidity()) {
+      isValid = false;
+    }
+  });
+
+  if (isValid) {
+    // display success message
+    const successMessage = document.getElementById("success-message");
+    successMessage.style.display = "block";
+    form.reset();
+  }
+}
